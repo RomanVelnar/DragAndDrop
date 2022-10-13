1a0682c24b5edc7f293f04ac56343ada6b75a0dc
Roman Velnar <roman.velnar@spikeglobal.com>
2022-10-13 12:42:46 +0100

    added the rendering project list logic

--- src/app.ts
+++ src/app.ts
@@ -56,6 +56,41 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// Project List Class
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -99,19 +134,19 @@ class ProjectInput {
 
     const titleValidatable: Validatable = {
       value: enteredTitle,
-      required: true
-    }
+      required: true,
+    };
     const descriptionValidatable: Validatable = {
       value: enteredDescription,
       required: true,
-      minLength: 5
-    }
+      minLength: 5,
+    };
     const peopleValidatable: Validatable = {
       value: +enteredPeople,
       required: true,
-      min: 1, 
-      max: 5
-    }
+      min: 1,
+      max: 5,
+    };
 
     if (
       !validate(titleValidatable) ||
@@ -152,3 +187,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
