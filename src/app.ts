0a12b0e8e8b3c6f8fd28df4a04258c336d1a2d42
Roman Velnar <roman.velnar@spikeglobal.com>
2022-10-20 16:23:58 +0100

    started with the drag and drop logic

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// Drag and Drop Interfaces
+
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus {
   Active,
@@ -155,14 +168,17 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // Project Item Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
   private project: Project;
 
   get people() {
     if (this.project.people === 1) {
-      return '1 person';
-    }else {
-      return `${this.project.people} people`
+      return "1 person";
+    } else {
+      return `${this.project.people} people`;
     }
   }
 
@@ -174,12 +190,22 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderContent();
   }
 
-  configure(): void {}
+  @autobind
+  dragStartHandler(event: DragEvent): void {
+    console.log('DragStart')
+  }
+  dragEndHandler(_: DragEvent): void {
+      console.log('DragEnd');
+  }
+
+  configure(): void {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
 
   renderContent(): void {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.people + ' assigned.';
+    this.element.querySelector("h3")!.textContent = this.people + " assigned.";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
