fd77e9d63395c9069fe215ac73068577b92d8115
Roman Velnar <roman.velnar@spikeglobal.com>
2022-10-20 14:39:49 +0100

    added the active and finished project filtering and removed the duplication

--- src/app.ts
+++ src/app.ts
@@ -135,7 +135,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
       projectState.addListener((projects: Project[]) => {
-        this.assignedProjects = projects
+        const relevantProjects = projects.filter(prj => {
+          if (this.type === 'active') {
+            return prj.status === ProjectStatus.Active
+          }
+            return prj.status === ProjectStatus.Finished          
+          });
+        this.assignedProjects = relevantProjects;
         this.renderProjects();
       });
 
@@ -145,6 +151,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = ''
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title
